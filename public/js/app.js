7ca8e73519c9b32f97dcd1962b4b9f73471ebb7e
Carl Hain <crhain@gmail.com>
2018-04-06 15:10:51 -0500

    Reorganize app.js to make code more readable

--- public/js/app.js
+++ public/js/app.js
@@ -1,75 +1,44 @@
-/*
-  1. I can add, subtract, divide, and multiply two numbers
-  2. I can clear the input field with a clear button
-  3. I can chain mathematical expressions together untill I hit the equal and get the
-     right result
-  4. Note: limit input size so we don't get display overflows
 
-  I need to:
-  1. get correct number or operator from each button press
-  2. output each key press to the display and clear it when either ac or ce button is pressed
-  3. create data structure to hold input, parse it to perfrom calc, and then output it to display on equal
-*/
-//create an object to perform calculator functions
 var calculator = (function(){
-  var power = true;
-  var MAX = 10;
+  var MAX = 15; //controls number of charcters allowed in display
+  var power = true;  
   var inputBuffer = "";
   var myCalc = {};
   var display = $(".calculator__display > p");
 
-  function addToBuffer(value){
-    if(inputBuffer.length < MAX){
-     return inputBuffer += value;
-    }
-  };
-  function clearBuffer(){
-    inputBuffer = "";
-  };
-  function updateDisplay(){
+  //////////////////////////////////////////////////////////////
+  //   PUBLIC METHODS                                         //
+  //////////////////////////////////////////////////////////////
 
-    if(arguments.length > 0){
-      //console.log("my arguments are: " + arguments[0]);
-      display.text(arguments[0]);
-    }
-    else if(inputBuffer.length === 0){
-      display.text("0");
-    }
-    else{
-      display.text(inputBuffer);
-    }
+  //Handle AC button clicks -- darkens display when turned off
+  myCalc.power = function(){
+    power = !power;
+    $("#display").toggleClass("calculator__display--on");    
+    this.clear();
+  }
+  //Clear calculator display
+  myCalc.clear = function(){
+    clearBuffer();
+    updateDisplay();
   };
-
+  //keypad input handler for calculator
   myCalc.input = function(value){
     if(power){
       addToBuffer(value);
       updateDisplay();
     }    
   };
-
-  myCalc.clear = function(){
-    clearBuffer();
-    updateDisplay();
-  };
-
-  myCalc.power = function(){
-    power = !power;
-    $("#display").toggleClass("calculator__display--on");    
-    this.clear();
-  }
-
+  //calculates values when EQUAL button is pressed
   myCalc.calculate = function(){
-    /*number.toExponential  on any answer greater than MAX*/
+    //number.toExponential on any answer greater than MAX
     if(power){
       try{
         var calc = new Function('return ' + inputBuffer + ';');
-        var answer = calc(); //no longer use eval(inputBuffer) because new Function offers better performance
-        //console.log(answer);
+        var answer = calc();         
         if(answer == 777){
           updateDisplay("Carl Hain");
           return 0;
-        }
-  
+        }  
         answer = answer.toString();
         var len = answer.length;
         if(len > MAX){
@@ -83,15 +52,46 @@ var calculator = (function(){
         updateDisplay("ERROR");
       }
     }
-    
   };
+
+  //////////////////////////////////////////////////////////////
+  //   PRIVATE METHODS                                        //
+  //////////////////////////////////////////////////////////////
+
+  //add values to inputBuffer
+  function addToBuffer(value){
+    if(inputBuffer.length < MAX){
+     return inputBuffer += value;
+    }
+  }
+  //clear inputBuffer
+  function clearBuffer(){
+    inputBuffer = "";
+  }
+  //update the display with contents of inputBuffer
+  function updateDisplay(){
+
+    if(arguments.length > 0){
+      //console.log("my arguments are: " + arguments[0]);
+      display.text(arguments[0]);
+    }
+    else if(inputBuffer.length === 0){
+      display.text("0");
+    }
+    else{
+      display.text(inputBuffer);
+    }
+  }          
   return myCalc;
 })();
-/*
-  Main function to run on document ready
-*/
+
+////////////////////////////////////////////////////////////////
+//   Main function to run on document ready                   //
+////////////////////////////////////////////////////////////////
 $("document").ready(function(){
+  //register calculator as moveable object using moveable.js
   registerMoveableObject(document.getElementById("calculator"));
+  //set display text to zero
   $(".calculator__display > p").text("0");
   //set up event handler for button clicks
   $(".calculator__btn").on("click", function(event){
@@ -102,4 +102,3 @@ $("document").ready(function(){
     else{ calculator.input(value); }
   });
 });
-
